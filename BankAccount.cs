0f929a90ff06ec287415da4dcd8e7d56551272e1
Othmane Zenzamy <59285391+DevopsZEN@users.noreply.github.com>
2020-02-16 01:36:38 +0100

    Initial

---
+++ BankAccount.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mySuperBank
+{
+    public class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance 
+        { 
+            get 
+            {
+                decimal balance = 0;
+                foreach(var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            }
+        }
+        private static int accountNumberSeed = 1234567890;
+        private List<Transaction> allTransactions = new List<Transaction>();
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial Balance");
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note) 
+        {
+            if(amount <= 0) 
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "The amount must be above 0");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if(amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "The amount must be a positive");
+            }
+            if(Balance - amount < 0)
+            {
+                throw new InvalidOperationException("You can't draw you balance don't have money to draw");
+            }
+            var withdraw = new Transaction(-amount, date, note);
+            allTransactions.Add(withdraw);
+        }
+
+        public string AccountHistory()
+        {
+            var report = new StringBuilder();
+            report.AppendLine("Date\t\tAmount\t\tNote");
+            foreach(var item in allTransactions)
+            {
+                report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{item.Notes}");
+            }
+            return report.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace mySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Kendra", 10000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}");
+
+            account.MakeWithdrawal(200, DateTime.Now, "Jackpot");
+            Console.WriteLine(account.Balance);
+
+            Console.WriteLine(account.AccountHistory());
+            //account.MakeDeposit(-300, DateTime.Now, "Yes this is work");
+        }
+    }
+}
